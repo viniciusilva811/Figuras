2561388ec533c431cbfc5f89851b726cb7eceb38
Vinícius Silva <vinisilva45@live.com>
2020-09-09 09:55:30 -0300

    Adicionar arquivos de projeto.

---
+++ Figuras/Entidades/Enum/Cor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figuras.Entidades.Enum
+{
+    enum Cor
+    {
+        Black,
+        Blue,
+        Red
+    }
+}
---
+++ Figuras/Entidades/Shape.cs
@@ -0,0 +1,19 @@
+﻿using Figuras.Entidades.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figuras.Entidades
+{
+    abstract class Shape
+    {
+        public Cor Cor { get; set; }
+
+        public Shape(Cor cor) 
+        {
+            Cor = cor;
+        }
+
+        public abstract double Area();
+    }
+}

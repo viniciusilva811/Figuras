99a661a36fe961baad178d2693088b3faf5da9f1
Vinícius Silva <vinisilva45@live.com>
2020-09-09 10:07:35 -0300

    Circulo completo.

---
+++ Figuras/Entidades/Circulo.cs
@@ -0,0 +1,22 @@
+﻿using Figuras.Entidades.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figuras.Entidades
+{
+    class Circulo : Shape
+    {
+        public  double  Raio { get; set; }
+
+        public Circulo(double raio, Cor cor) : base(cor) 
+        {
+            Raio = raio;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Raio * Raio;
+        }
+    }
+}
